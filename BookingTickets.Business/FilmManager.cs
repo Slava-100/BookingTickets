9bc890c179e45f42ee376756e1cd21cf7b0e9506
Svyatoslav <svat.strel.2001@gmail.com>
2024-04-22 04:23:49 +0300

    Add FilmController/Manager/Repository

---
+++ BookingTickets.Business/FilmManager.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BookingTickets.Core.Contracts;
+using BookingTickets.Core.Models.BLL;
+using BookingTickets.Core.Models.Dtos;
+
+namespace BookingTickets.Business;
+
+public class FilmManager(IRepository<FilmDto> filmRepository, IMapper mapper) : IFilmManager
+{
+    public async Task<IEnumerable<Film>> GetFilmsAsync()
+    {
+        var callback = await filmRepository.GetAllAsync();
+        var films = mapper.Map<IEnumerable<Film>>(callback);
+
+        return films;
+    }
+}
